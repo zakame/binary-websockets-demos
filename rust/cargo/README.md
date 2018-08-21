446e3a44a57afbbd431773eb0b0ff212090c620c
Zak B. Elep <zakame@zakame.net>
2018-08-21 15:05:24 +0800

    Add Rust demo (using Cargo)
    This is another one greatly modified from the current demo, as we use
    WS-RS instead of rust-websockets, to support a more straightforward way
    to program and implement the tick stream with timeout.

---
+++ rust/cargo/README.md
@@ -0,0 +1,6 @@
+# WebSockets demo for Rust (using Cargo)
+
+This needs [Cargo](https://doc.rust-lang.org/stable/cargo/):
+
+    $ cargo build
+    $ target/debug/ticks
---
+++ rust/cargo/src/main.rs
@@ -0,0 +1,30 @@
+extern crate ws;
+
+use ws::util::Token;
+use ws::{connect, CloseCode, Handler, Handshake, Message, Result, Sender};
+
+const URL: &'static str = "wss://ws.binaryws.com/websockets/v3?app_id=1089";
+
+struct Client {
+    ws: Sender,
+}
+
+impl Handler for Client {
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        try!(self.ws.send("{\"ticks\": \"R_100\"}"));
+        self.ws.timeout(10_000, Token(1))
+    }
+
+    fn on_timeout(&mut self, _: Token) -> Result<()> {
+        self.ws.close(CloseCode::Away)
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("ticks update: {}", msg);
+        Ok(())
+    }
+}
+
+fn main() {
+    connect(URL, |ws| Client { ws: ws }).unwrap()
+}
