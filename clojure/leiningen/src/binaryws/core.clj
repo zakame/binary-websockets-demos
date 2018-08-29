52b0cd173ee6e9d4576357c609e9531e4e6e5c70
Zak B. Elep <zakame@zakame.net>
2018-08-29 15:55:26 +0800

    Clojure example: fix tick output
    Make it the same as with the other examples.

--- clojure/leiningen/src/binaryws/core.clj
+++ clojure/leiningen/src/binaryws/core.clj
@@ -8,7 +8,7 @@
   [& args]
   (def socket
     (ws/connect "wss://ws.binaryws.com/websockets/v3?app_id=1089"
-      :on-receive #(prn (parse-string %))))
+      :on-receive #(println "ticks update:" %)))
   (ws/send-msg socket (generate-string {:ticks "R_100"}))
   (Thread/sleep 10000)
   (ws/close socket))
