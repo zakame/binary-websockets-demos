a2dbc72a975baa19da0354593acc2e06788544ee
Zak B. Elep <zakame@zakame.net>
2018-08-30 18:44:02 +0800

    Add PHP demo (using Pawl)
    This uses Pawl instead of phpws as only WebSocket client functionality
    is needed here.

---
+++ php/README.md
@@ -0,0 +1,6 @@
+# WebSockets demo for PHP (using Pawl)
+
+This needs [Composer](https://getcomposer.org):
+
+    $ composer install
+    $ php ticks.php
---
+++ php/ticks.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loop = \React\EventLoop\Factory::create();
+$connector = new \Ratchet\Client\Connector($loop);
+
+$connector('wss://ws.binaryws.com/websockets/v3?app_id=1089')->then(function ($conn) {
+    $conn->on('message', function ($msg) {
+        echo "ticks update: {$msg}\n";
+    });
+
+    $conn->send("{\"ticks\": \"R_100\"}");
+});
+
+$loop->addTimer(10, function () use ($loop) {
+    $loop->stop();
+});
+
+$loop->run();
