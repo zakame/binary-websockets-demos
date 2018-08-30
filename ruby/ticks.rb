0c2d8798654c88d90b8fc4b112bfc615bbea32cb
Zak B. Elep <zakame@zakame.net>
2018-08-31 00:45:19 +0800

    Ruby example: fix style
    - use do...end instead of braces
    - remove redundant braces on JSON.generate() argument
    - trim long line on ws variable initialization

--- ruby/ticks.rb
+++ ruby/ticks.rb
@@ -2,11 +2,12 @@ require 'faye/websocket'
 require 'eventmachine'
 require 'json'
 
-EM.run {
-  ws = Faye::WebSocket::Client.new('wss://ws.binaryws.com/websockets/v3?app_id=1089')
+EM.run do
+  api_url = 'wss://ws.binaryws.com/websockets/v3?app_id=1089'
+  ws = Faye::WebSocket::Client.new(api_url)
 
   ws.on :open do
-    ws.send(JSON.generate({ticks: 'R_100'}))
+    ws.send(JSON.generate(ticks: 'R_100'))
   end
 
   ws.on :message do |msg|
@@ -16,4 +17,4 @@ EM.run {
   EM.add_timer(10) do
     EM.stop_event_loop
   end
-}
+end
