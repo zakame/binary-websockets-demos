d55fbd658f4876c10144f371658f5e39af99dd1f
Zak B. Elep <zakame@zakame.net>
2020-10-13 12:51:58 +0800

    Rust example: Update for newer Rust versions
    Thanks, `cargo clippy --all-targets`!
    
    (We're probably due replacing ws.rs with something else as well, let's
    see later...)

--- rust/cargo/src/main.rs
+++ rust/cargo/src/main.rs
@@ -3,7 +3,7 @@ extern crate ws;
 use ws::util::Token;
 use ws::{connect, CloseCode, Handler, Handshake, Message, Result, Sender};
 
-const URL: &'static str = "wss://ws.binaryws.com/websockets/v3?app_id=1089";
+const URL: &str = "wss://ws.binaryws.com/websockets/v3?app_id=1089";
 
 struct Client {
     ws: Sender,
@@ -11,7 +11,7 @@ struct Client {
 
 impl Handler for Client {
     fn on_open(&mut self, _: Handshake) -> Result<()> {
-        try!(self.ws.send("{\"ticks\": \"R_100\"}"));
+        (self.ws.send("{\"ticks\": \"R_100\"}"))?;
         self.ws.timeout(10_000, Token(1))
     }
 
@@ -26,5 +26,5 @@ impl Handler for Client {
 }
 
 fn main() {
-    connect(URL, |ws| Client { ws: ws }).unwrap()
+    connect(URL, |ws| Client { ws }).unwrap()
 }
